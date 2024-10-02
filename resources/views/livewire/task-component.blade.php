6d9e5478e45e2647dbc0e13a478efc748a92fbb2
Sanqm <sandra.queiamdelos@gmail.com>
2024-10-02 18:30:49 +0200

    retocando h1

--- resources/views/livewire/task-component.blade.php
+++ resources/views/livewire/task-component.blade.php
@@ -10,7 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="font-2xl text-purple-600 mb-4">Bienvenido al Gestor de Tareas</h1>
+                    <h1 class="text-2xl text-purple-600 mb-4">Bienvenido al Gestor de Tareas</h1>
                     <div class="relative overflow-x-auto">
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
